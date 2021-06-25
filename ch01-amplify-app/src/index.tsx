81e5cecdf27b4d2817f7cee10f691e399607e820
t2 <oo_ri@naver.com>
2021-06-26 02:06:02 +0900

    Amplify Auth Setup and POC

--- ch01-amplify-app/src/index.tsx
+++ ch01-amplify-app/src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+
 import App from './App';
+
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
