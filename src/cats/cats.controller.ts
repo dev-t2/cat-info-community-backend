0dcde9ce4d0b654b7edfb6d5022af5d5ac957afe
dev-t2 <oo_ri@naver.com>
2022-09-25 08:48:06 +0900

    Cats App Backend Code Refactoring

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './cats.dto';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  createCat(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.createCat(createCatDto);
+  }
+
+  @Get()
+  findCats() {
+    return this.catsService.findCats();
+  }
+
+  @Get(':id')
+  findCat(@Param('id') id: string) {
+    return this.catsService.findCat(id);
+  }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateCatDto } from './cats.dto';
+
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  createCat(createCatDto: CreateCatDto) {
+    return createCatDto;
+  }
+
+  findCats() {
+    return 'findAll';
+  }
+
+  findCat(id: string) {
+    return `findOne ${id}`;
+  }
+}
