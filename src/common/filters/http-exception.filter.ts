d57efbcb878fee67b42ab460211fafbb9619e9da
dev-t2 <oo_ri@naver.com>
2022-09-25 10:41:39 +0900

    HTTP Exception Filter Update

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,29 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const error = exception.getResponse() as string | { message: string | string[] };
+
+    if (typeof error === 'string') {
+      return response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message: error,
+      });
+    }
+
+    return response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: error.message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
