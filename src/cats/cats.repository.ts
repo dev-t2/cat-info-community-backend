c70d420a9bfb1748961dd7ce291edacc738c4821
dev-t2 <oo_ri@naver.com>
2022-09-27 23:47:12 +0900

    Cats Repository Pattern Update

---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Cat } from './cats.schema';
+import { SignUpDto } from './cats.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existsEmail(email: string) {
+    return await this.catModel.exists({ email });
+  }
+
+  async existsNickname(nickname: string) {
+    return await this.catModel.exists({ nickname });
+  }
+
+  async signUp(signUpDto: SignUpDto) {
+    return await this.catModel.create(signUpDto);
+  }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,23 +1,21 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import bcrypt from 'bcrypt';
 
-import { Cat } from './cats.schema';
+import { CatsRepository } from './cats.repository';
 import { SignUpDto } from './cats.dto';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp({ email, nickname, password }: SignUpDto) {
-    const isExistsEmail = await this.catModel.exists({ email });
+    const isExistsEmail = await this.catsRepository.existsEmail(email);
 
     if (isExistsEmail) {
       throw new BadRequestException('이미 존재하는 이메일입니다.');
     }
 
-    const isExistsNickname = await this.catModel.exists({ nickname });
+    const isExistsNickname = await this.catsRepository.existsNickname(nickname);
 
     if (isExistsNickname) {
       throw new BadRequestException('이미 존재하는 닉네임입니다.');
@@ -25,6 +23,6 @@ export class CatsService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await this.catModel.create({ email, nickname, password: hashedPassword });
+    await this.catsRepository.signUp({ email, nickname, password: hashedPassword });
   }
 }
