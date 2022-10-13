66759df94324da3559a273c38ded79832c7b5d52
dev-t2 <oo_ri@naver.com>
2022-10-14 00:42:12 +0900

    Cats and Comments Module DI Bug Fix

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,16 +1,16 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { CatsRepository } from './cats.repository';
 
 @Module({
   imports: [
-    PrismaModule,
     forwardRef(() => AuthModule),
+    PrismaModule,
     MulterModule.register({ dest: './upload' }),
   ],
   controllers: [CatsController],
--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CatsModule } from 'src/cats/cats.module';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { CommentsRepository } from './comments.repository';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CatsModule],
   controllers: [CommentsController],
   providers: [CommentsService, CommentsRepository],
 })
