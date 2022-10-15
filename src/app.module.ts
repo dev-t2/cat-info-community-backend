0283b7913a6d727903011925755caf572110bbcc
dev-t2 <oo_ri@naver.com>
2022-10-15 23:03:17 +0900

    AWS SDK Install and AWS Module Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { AwsModule } from './aws/aws.module';
 import { CatsModule } from './cats/cats.module';
 import { CommentsModule } from './comments/comments.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule,
+    AwsModule,
     CatsModule,
     CommentsModule,
   ],
